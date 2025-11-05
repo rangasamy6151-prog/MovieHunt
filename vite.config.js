d10e43607d0138036b317d2fc6177a66eea1b3f5
rangasamy6151-prog <rangasamy6151@gmail.com>
2025-11-05 12:40:30 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MovieHunt/',
+  base: '/MovieHunt/', 
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-  },
+    assetsDir: 'assets'
+  }
 })
